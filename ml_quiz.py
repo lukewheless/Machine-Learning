d558ffcda711adb10e6e07cbf1d6c33da97969fa
Wheless <Luke_Wheless@baylor.edu>
2020-11-16 18:22:36 -0600

    enter

--- ml_quiz.py
+++ ml_quiz.py
@@ -1,68 +1,36 @@
-from sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+knn = KNeighborsClassifier()
 
 #Data Frames
 classes =  pd.read_csv("animal_classes.csv", usecols=["Class_Number", "Class_Type"])
-df1 = pd.DataFrame(classes)
-train =  pd.read_csv("animals_train.csv")
-df2 = pd.DataFrame(train)
+classes_df = pd.DataFrame(classes)
+train =  pd.read_csv("animals_train.csv") #df without without target
+train_df = pd.DataFrame(train)
 test =  pd.read_csv("animals_test.csv")
+test_df = pd.DataFrame(test)
 
-#New Column
-df1.sort_index().sort_index(axis=1) == df2.sort_index().sort_index(axis=1)
+#train.rename(columns={"class_number":"Class_Number"}, inplace=True)
+#merge = pd.merge(train,classes, how='left', on='Class_Number')
 
-df2['Target'] = np.where(df1['Class_Number'] == df2['class_number'], df1['Class_Type'])
-                                                   
-print(train)          
+train = train_df.iloc[:,:16] #without target class_type
+target = train_df.iloc[:,16] #with target
+test = test_df.iloc[:,1:] #without animal_name
+name = test_df.iloc[:,0] #With just names
+types = classes_df.iloc[:,1]
 
+#Train Model
+knn.fit(X=train, y=target)
+predicted = knn.predict(X=test) #predicts target
 
-'''
-for i in train["class_number"]:
-    if i == 1:
-        train["Target"] = "Mammal"
-    if i == 2:
-        train["Target"] = "Bird"
-    if i == 3:
-        train["Target"] = "Reptile"
-    if i == 4:
-        train["Target"] = "Fish"
-    if i == 5:
-        train["Target"] = "Amphibian"
-    if i == 6:
-        train["Target"] = "Invertebrate"
+l = 0
+outfile = open("predictions.csv", 'w')
+h = 'animal_name, predicted'
+outfile.write(h + '\n')
 
-print(train)
-
-
-conditions = ["Mammal" if i == 1
-                    "Bird" if i == 2
-                    "Reptile" elif i == 3
-                    "Fish" elif i == 4
-                    "Amphibian" elif i == 5
-                    "Bug" elif i == 6
-                    "Invertebrate" elif i == 7
-                    for i in train["class_number"]]
-train["Target"] = np.
-
-x_train, x_test, y_train, y_test = train_test_split(train.Target.values, test.Name.values, random_state=11)
-
-lr = LinearRegression()
-
-lr.fit(X=x_train, y=y_train)
-
-predicted = lr.predict(x_test)
-
-expected = y_test
-
-for p,e in zip(predicted[::5], expected[::5]):
-    #print(f"predicted: {p: .2f}, expected: {e: .2f}")
-
-df = pd.DataFrame(predicted, expected, columns = "Predicted", "Expected")
-df.to_csv("predictions.csv")
-print('Done!')
-'''
+for p in predicted:
+    line = f"{name[l]},{types[int(p)-1]}\n"
+    print(line)
+    outfile.write(line)
+    l += 1
